f023682862ff110bff708c5675d1fc8967fceb38
Miguel <miguel.aparicio3@utp.ac.pa>
2026-05-03 17:13:29 -0500

    fix: remove database.properties from tracking

--- src/main/java/com/miguel/gastos/MainApp.java
+++ src/main/java/com/miguel/gastos/MainApp.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class MainApp {
         public static void main(String[] args) {
             GastoDAO dao = new GastoDAOImpl();
+
             List<Gasto> gastos = dao.listarTodos();
             gastos.forEach(System.out::println);
         }
